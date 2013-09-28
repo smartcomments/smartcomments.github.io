8bac8bb92cf1dac48f974490374b68b27436fe60
dariel <darielnoel@gmail.com>
2013-09-27 22:01:13 -0430

    carlos changes

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,75 +1,26 @@
-var x;
-x=$(document);
-x.ready(inicializarEventos);
-var myCodeMirror = {};
+$(document).ready(inicializarEventos);
 
 function inicializarEventos() {
 	navbar();
-	var demo_value = '//Paste or Write your code here for testing...\n\n'+
-					'var someUncommentFunction = function(a, b){\n'+
-					'	return true;\n'+
-					'}\n\n'+
-					'function otherUncommentFunction(){\n'+
-						'	var x = 1,\n'+
-					        '		y = 1;\n'+
-					   	'	return x;\n'+
-					'}\n\n'+
-					'var someObject = {\n'+
-					  '	anotherUncommentFunction: function(x, y){\n'+
-					  	'		return x;\n'+
-					  '	}\n'+
-					'}';
-
-	myCodeMirror = CodeMirror($('div.demo')[0], {
-  		value: demo_value,
-  		mode:  "javascript"
-	});
+	// $(".generate").click(function() {
+	// };
 }
-
-function refresh(){
-     location.reload();
-}
-
+// function refresh(){
+//      location.reload();             
+// }
 function navbar(){
-	$(".home").click(function() {
-	 $("#li-home").attr('class','active');
-	 $("#li-installation").attr('class','');
-	 $("#li-usage").attr('class','');
-	 $("#li-features").attr('class','');
-	 $("#li-about").attr('class','');
-	 
-	});
-
-    $(".installation").click(function() {
-	 $("#li-home").attr('class','');
-	 $("#li-installation").attr('class','active');
-	 $("#li-usage").attr('class','');
-	 $("#li-features").attr('class','');
-	 $("#li-about").attr('class','');
-	});
-
-	$(".usage").click(function() {
-	 $("#li-home").attr('class','');
-	 $("#li-installation").attr('class','');
-	 $("#li-usage").attr('class','active');
-	 $("#li-features").attr('class','');
-	 $("#li-about").attr('class','');
-	});
-
-	$(".features").click(function() {
-	 $("#li-home").attr('class','');
-	 $("#li-installation").attr('class','');
-	 $("#li-usage").attr('class','');
-	 $("#li-features").attr('class','active');
-	 $("#li-about").attr('class','');
-	});
-
-	$(".about").click(function() {
-	 $("#li-home").attr('class','');
-	 $("#li-installation").attr('class','');
-	 $("#li-usage").attr('class','');
-	 $("#li-features").attr('class','');
-	 $("#li-about").attr('class','active');
+	$(".jquery").click(function() {
+		var CLASS = 'class',
+			i = 1,
+			id = $(this).attr('id');
+		$('#'+id).attr(CLASS,'active');
+		 for (i; i <= 5; i++) {
+			if (i==id) {
+		 		continue;	
+		 	}
+		 	else {
+		 		$('#'+i).attr(CLASS,'');
+		 	}	 
+		 }
 	});
 }
-
