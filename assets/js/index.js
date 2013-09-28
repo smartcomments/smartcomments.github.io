82996501bf67cfb0ce39073b8b025c0f60e6597b
dariel <darielnoel@gmail.com>
2013-09-27 22:22:30 -0430

    carlos integration

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,13 +1,29 @@
 $(document).ready(inicializarEventos);
 
+var myCodeMirror = {};
+
 function inicializarEventos() {
 	navbar();
-	// $(".generate").click(function() {
-	// };
+	var demo_value = '//Paste or Write your code here for testing...\n\n'+
+					'var someUncommentFunction = function(a, b){\n'+
+					'	return true;\n'+
+					'}\n\n'+
+					'function otherUncommentFunction(){\n'+
+						'	var x = 1,\n'+
+					        '		y = 1;\n'+
+					   	'	return x;\n'+
+					'}\n\n'+
+					'var someObject = {\n'+
+					  '	anotherUncommentFunction: function(x, y){\n'+
+					  	'		return x;\n'+
+					  '	}\n'+
+					'}';
+
+	myCodeMirror = CodeMirror($('div.demo')[0], {
+  		value: demo_value,
+  		mode:  "javascript"
+	});
 }
-// function refresh(){
-//      location.reload();             
-// }
 function navbar(){
 	$(".jquery").click(function() {
 		var CLASS = 'class',
