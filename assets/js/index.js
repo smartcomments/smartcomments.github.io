35617fd9bef8708cea69dd8558521afb57e645c9
dariel <darielnoel@gmail.com>
2013-09-27 21:28:22 -0430

    sintax higliting ready

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,16 +1,33 @@
 var x;
 x=$(document);
 x.ready(inicializarEventos);
+var myCodeMirror = {};
 
 function inicializarEventos() {
 	navbar();
-	// $(".generate").click(function() {
-		
-	// };
+	var demo_value = '//Paste or Write your code here for testing...\n\n'+
+					'var someUncommentFunction = function(a, b){\n'+
+					'	return true;\n'+
+					'}\n\n'+
+					'function otherUncommentFunction(){\n'+
+						'	var x = 1,\n'+
+					        '		y = 1;\n'+
+					   	'	return x;\n'+
+					'}\n\n'+
+					'var someObject = {\n'+
+					  '	anotherUncommentFunction: function(x, y){\n'+
+					  	'		return x;\n'+
+					  '	}\n'+
+					'}';
+
+	myCodeMirror = CodeMirror($('div.demo')[0], {
+  		value: demo_value,
+  		mode:  "javascript"
+	});
 }
 
 function refresh(){
-     location.reload();             
+     location.reload();
 }
 
 function navbar(){
@@ -55,3 +72,4 @@ function navbar(){
 	 $("#li-about").attr('class','active');
 	});
 }
+
